7612a49926163c975c7da89c6c8fbb1b315abc4c
Aman21-dev <devopsjava21@outlook.com>
2021-06-13 18:30:10 +0530

    edited

---
+++ Untitled2.ps1
@@ -0,0 +1,10 @@
+﻿Connect-AzAccount
+
+
+$resourceGroup = "myResourceGroup"
+$resourceGroup.id
+
+
+New-AzResourceGroupDeployment  -ResourceGroupName $resourceGroup -TemplateFile C:\Users\User\Downloads\ExportedTemplate-Microsoft.Web-WebApp-Portal-2920b73b-8684\template.json  -TemplateParameterFile C:\Users\User\Downloads\ExportedTemplate-Microsoft.Web-WebApp-Portal-2920b73b-8684\parameters.json
+
+Get-AzResourceGroup myResourceGroup
--- stopvm.ps1
+++ stopvm.ps1
@@ -1,6 +1,22 @@
-﻿Stop-AzVM -Name $vm.Name -ResourceGroup $vm.ResourceGroupName
+﻿#To get VM details
+
+$vm = (Get-AzVM -Name "AmanVm" -ResourceGroupName "myResourceGroup")
+
+
+Stop-AzVM -Name $vm.Name -ResourceGroup $vm.ResourceGroupName
+
+Start-AzVM -Name $vm.Name -ResourceGroup $vm.ResourceGroupName
 
 Remove-AzVM -Name $vm.Name -ResourceGroup $vm.ResourceGroupName
 
+$vm | Remove-AzNetworkInterface –Force
 
-Get-AzResource -ResourceGroupName $vm.ResourceGroupName | ft
+Get-AzDisk -ResourceGroupName $vm.ResourceGroupName -DiskName $vm.StorageProfile.OSDisk.Name | Remove-AzDisk -Force
+
+Get-AzVirtualNetwork -ResourceGroup $vm.ResourceGroupName | Remove-AzVirtualNetwork -Force
+
+Get-AzNetworkSecurityGroup -ResourceGroup $vm.ResourceGroupName | Remove-AzNetworkSecurityGroup -Force
+
+Get-AzPublicIpAddress -ResourceGroup $vm.ResourceGroupName | Remove-AzPublicIpAddress -Force
+
+Get-AzResource -ResourceGroupName $vm.ResourceGroupName | Format-Table
